2f1b180dc7e890fc62a038f57a4f223a13d3d46b
spking <solvi1999@gmail.com>
2020-02-03 14:52:06 +0000

    add classes file

--- classes.py
+++ classes.py
@@ -6,5 +6,15 @@ from termcolor import cprint
 
 
 class Deck:
-    def __init__(self):
-        pass
+    def __init__(self, t, n):
+        self.tegund = t
+        self.numer = n
+
+    def Skapastokk(self, t, listi):
+        for x in range(1, 14):
+            t1 = Deck(t, x)
+            listi.append(t1)
+        return listi
+
+    def __str__(self):
+        return self.tegund +" "+ str(self.numer)
