4679980f761188976af855a84ab294013c937138
spking <solvi1999@gmail.com>
2020-02-04 09:46:44 +0000

    added bot file and main file

---
+++ botprofile.py
@@ -0,0 +1,7 @@
+# Solvi Scheving Pálsson#
+# Bot logic file #
+# 03/02/2019 #
+
+class Bot:
+    def __init__(self, d):
+        self.deck = d
---
+++ main.py
@@ -0,0 +1,7 @@
+# Sölvi Scheving Pálsson #
+# 04/02/2020 #
+
+import botprofile
+import classes
+
+
